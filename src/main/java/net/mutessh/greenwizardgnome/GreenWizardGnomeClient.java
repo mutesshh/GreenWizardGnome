344e074026af8d13d02fea96ef601708a92a092d
Javier Garcia de villa <javier.gdv.99@gmail.com>
2024-08-13 15:55:23 +0200

    Initial commit

---
+++ src/main/java/net/mutessh/greenwizardgnome/GreenWizardGnomeClient.java
@@ -0,0 +1,10 @@
+package net.mutessh.greenwizardgnome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GreenWizardGnomeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mutessh/greenwizardgnome/GreenWizardGnomeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mutessh.greenwizardgnome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GreenWizardGnomeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
